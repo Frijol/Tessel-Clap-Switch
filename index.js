b72dc4658ca6443ad7eb21139c4329e9d8f69660
Kelsey <ifoundthemeaningoflife@gmail.com>
2014-07-29 10:54:29 -0700

    update to production API

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // toggles relay 1 when that trigger is hit.
 
 var tessel = require('tessel');
-var relay = require('relay-mono').use(tessel.port('A'));
-var ambient = require('ambient-attx4').use(tessel.port('B'));
+var relay = require('relay-mono').use(tessel.port['A']);
+var ambient = require('ambient-attx4').use(tessel.port['B']);
 
 // Sound level (0-1) needed to trigger. You may need to adjust this.
 var triggerVal = 0.22;
@@ -11,7 +11,7 @@ var triggerVal = 0.22;
 // When the module is connected
 ambient.on('ready', function () {
   // Set the sound trigger
-  ambient.setSoundTrigger(0.22);
+  ambient.setSoundTrigger(triggerVal);
 
   // When the sound trigger is reached
   ambient.on('sound-trigger', function triggerHit() {
